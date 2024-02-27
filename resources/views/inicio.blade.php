3e68f0ab80ac1167aa411a2f7dcf4aaf5799fa42
Alexis <aivega@espe.edu.ec>
2024-02-27 10:01:16 -0500

    act

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -7,63 +7,73 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Document</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        .header-container {
-            position: fixed;
-            width: 100%;
-            top: 0;
-            left: 0;
-            background-color: #FFFFFF;
-            z-index: 1000;
-            /* Asegura que el contenedor esté por encima de otros elementos */
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-        }
-        .header {
-            text-align: center;
-            color: green;
-        }
-        .header img {
-            width: 572.09px;
-            height: 100px;
-            margin: 0px 0px 15px;
-        }
-        .navbar {
-            overflow: hidden;
-            display: flex;
-            justify-content: space-around;
-            padding: 20px;
-        }
+       body {
+    font-family: Arial, sans-serif;
+}
+.header {
+    text-align: center;
+    color: green;
+}
+.header img {
+    width: 572.09px;
+    height: 100px;
+    margin: 0px 0px 15px;
+}
+body {
+    font-family: Arial, sans-serif;
+}
+.navbar {
+    background-color: #FFFFFF;
+    overflow: hidden;
+    display: flex;
+    justify-content: space-around;
+    padding: 20px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+.navbar a {
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    border-radius: 25px;
+    font-size: 17px;
+}
+.navbar a:hover {
+    background-color: #ddd;
+    color: black;
+}
 
-        .navbar a {
-            color: black;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-            border-radius: 25px;
-            font-size: 14px;
-        }
+.call-icon {
+	float: right;
+  }
+  
 
-        .navbar a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-
-        .call-icon {
-            float: right;
-        }
-        .footer1 {
-    background-color: #123456;
+/* Pie de página */
+.footer1{
+    background-color: #333333;
     color: #fff;
     padding: 20px;
+    text-align: center;
 }
+
 .footer2 {
-    background-color: #333333; /* Change this to your desired color */
+    background-color: #333333;
     color: #fff;
     padding: 20px;
+    text-align: center;
+}
+.footer3{
+    background-color: #33333333;
+    color: #ffff;
+    padding: 20px;
+    text-align: center;
 }
 
+.footer2 {
+    background-color: #123456; /* Cambiar al color de fondo deseado */
+}
+
+/* Estilos adicionales para el pie de página */
 .footer-container {
     display: flex;
     justify-content: space-between;
@@ -77,11 +87,6 @@
 .footer-bottom {
     text-align: center;
     margin-top: 20px;
-}
-.footer-bottom1 {
-    background-color: #f5f5f5; /* Este es un color de fondo más claro */
-    color: #333; /* Asegúrate de cambiar también el color del texto para que sea legible */
-    padding: 20px;
 }
 
     </style>
@@ -107,11 +112,25 @@
             <img src="imagen/imagen1.jpg" >
         </div>
         <div class="drt-control control-left" id="lft-control">
-            <div class="drt-control control-right" id="rht-control">></div>
+            <div class="drt-control control-right" id="rht-control"></div>
             <ul class="slider-controls" id="slider-controls"></ul>
             <div class="tempo-bar" id="barra"></div>
         </div>
     </section>
+    <footer class="footer3">
+<div class="container">
+    <div class="content-wrapper">
+        <h1>Coworking Espe</h1>
+        <h2>Sede Latacunga</h2>
+        <div class="center-text">
+            <p style="font-size: 24px;">La Universidad de las Fuerzas Armadas ESPE continúa destacándose como un 
+            referente en el ámbito académico al ofrecer un espacio de coworking que va más allá de las convenciones tradicionales.</p>
+            <p style="font-size: 24px;">Este innovador espacio se erige como un epicentro de colaboración y desarrollo, donde se fusionan la excelencia académica, la innovación y el espíritu emprendedor en un entorno dinámico y colaborativo.</p>
+        </div>
+    </div>
+</div>
+</footer>
+
     <footer class="footer1">
     <div class="footer-container">
         <div class="footer-column">
@@ -139,6 +158,11 @@
             <h3>DEPARTAMENTOS LATACUNGA</h3>
             <p>Ciencias Exactas</p>
             <p>Energía y Mecánica</p>
+            <p>Ciencias Económicas Administrativa y de Comercio</p>
+            <p>Energía y Mecanica</p>
+            <p>Seguridad y Defensa</p>
+            <p>Ciencias Humanas y Sociales</p>
+            <p>Ciencias de la Computación</p>
             <!-- Add more departments as needed -->
         </div>
     </div>
