b1adfee8e511ff1eb8611c60a3b597b17f621d5c
Nnicole12 <160640122+Nnicole12@users.noreply.github.com>
2024-02-27 08:15:39 -0500

    Update Ubicacion.blade.php

--- resources/views/Ubicacion.blade.php
+++ resources/views/Ubicacion.blade.php
@@ -115,7 +115,12 @@
 
     <div class="rectangulo">
 
-    </div>
+    </div><br><br><br>
+    <div class="container">
+  <div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d997.3215262476521!2d-78.61240942702352!3d-0.9358572239899882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d4610f5d33a74d%3A0x408aab614807127a!2sUniversidad%20de%20las%20Fuerzas%20Armadas%20ESPE%20Sede%20Latacunga%20(campus%20centro)!5e0!3m2!1ses!2sec!4v1707745243432!5m2!1ses!2sec" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+</div>
 
     
 
